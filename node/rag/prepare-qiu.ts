5035655c23631e6adc0d83581a4c84b099e08d36
huccct <ucccth@gmail.com>
2025-06-01 19:38:03 +0100

    chore: learn rag

---
+++ node/rag/prepare-qiu.ts
@@ -0,0 +1,31 @@
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import "dotenv/config";
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const run = async () => {
+  const baseDir = __dirname;
+
+  const loader = new TextLoader(path.join(baseDir, "../../data/qiu.txt"));
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const splitDocs = await splitter.splitDocuments(docs);
+
+  const embeddings = new OpenAIEmbeddings();
+  const vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
+
+  await vectorStore.save(path.join(baseDir, "../../db/qiu"));
+};
+
+run();
